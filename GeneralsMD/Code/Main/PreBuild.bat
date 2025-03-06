6b6e77b0e28718a57f590ebdfb73583c6ef23547
Вячеслав Грасюк <slava_agent@mail.ru>
2025-03-06 23:13:00 +0200

    Fix warnings at version headers generation caused by updated solution; make version headers to be out-of-tree, same as binary build artefacts

--- GeneralsMD/Code/Main/PreBuild.bat
+++ GeneralsMD/Code/Main/PreBuild.bat
@@ -1,7 +1,9 @@
 :: 1st arg 	- directory with tools binaries
+echo on
+echo Incrementing version numbers held in GeneratedVersion.h BuildVersion.h
 @echo off
 set GeneratedDir=%1..\..\gen\include\
-mkdir %GeneratedDir%
+if not exist %GeneratedDir% mkdir %GeneratedDir%
 %1\versionUpdate.exe %GeneratedDir%GeneratedVersion.h
 %1\buildVersionUpdate.exe %GeneratedDir%BuildVersion.h
 exit 0
